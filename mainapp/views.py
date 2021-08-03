5eecad1fce69d1a59927eac37b0d64a74de301c3
Dias Kadyrov <kadirov.dias@mail.ru>
2021-08-04 03:15:54 +0600

    gggggggggg

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-<<<<<<< HEAD
 from django.views.generic import DetailView
 
 from .models import *
@@ -10,15 +9,6 @@ def index(request):
     tiles = LatestProducts.objects.get_products_for_main_page('tile')
     stairs =  LatestProducts.objects.get_products_for_main_page('stair')
     return render(request, 'main/index.html', {'tiles': tiles, 'stairs':stairs})
->>>>>>> b08b45918ea7fce339bd8858c76e1ff09a7a0514
-=======
-from django.views import generic
-from django.views.generic.detail import DetailView
-from .models import *
-
-def index(request):
-    return render(request, 'main/index.html')
->>>>>>> ee537a309fb7aa10c57ba6c58d0c8e658749658a
 
 def about(request):
     return render(request, 'main/about.html')
