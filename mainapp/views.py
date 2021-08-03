91105cdd45e938a0c3740007012c18e157d6c9d5
ruslan.sibgatulin05@gmail.com <ruslan.sibgatulin05@gmail.com>
2021-08-04 03:07:26 +0600

    added-views list

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,29 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.generic import DetailView
-
-from .models import Tile
+from django.views import generic
+from .models import *
 
 def index(request):
-    # тут вернем шаблон
-    
     return render(request, 'main/index.html')
 
 def about(request):
     return render(request, 'main/about.html')
 
-class ProductDetailsView(DetailView):
-    #словарь для продуктов
+class TileListView(generic.ListView):
 
-    CT_MODEL_CLASS={
-        'tiles': Tile   
-    }
+    model = Tile
 
-    def dispatch(self, request, *args, **kwargs):
-        self.model = self.CT_MODEL_CLASS[kwargs.get('ct_model')]
-        self.queryset = self.model._base_manager.all()
-        return super().dispatch(request, *args, **kwargs)
+    def get_queryset(self):
+        return Tile.objects.order_by('title')
 
-    content_object_name = 'product'
-    template_name = 'product_detail.html'
-    slug_url_kwargs = 'slug'
+
+class StairListView(generic.ListView):
+
+    model = Stair
+
+    def get_queryset(self):
+        return Stair.objects.order_by('title')
