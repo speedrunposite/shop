32080aae673e77d48b9b44a5c6638a7a93936295
ruslan.sibgatulin05@gmail.com <ruslan.sibgatulin05@gmail.com>
2021-08-04 03:28:13 +0600

    ---

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from . import views
-from .views import ProductDetailsView
+from .views import *
 
 
 urlpatterns = [
     path('', views.index, name="home"),
     path('about-us', views.about, name="about"),
-    path('products/<str:ct_model>/<str:slug>/', ProductDetailsView.as_view(), name='product_detail')
+    # path('products/<str:slug>/<str:slug>/', ProductDetailsView.as_view(), name='product_detail')
 ]
