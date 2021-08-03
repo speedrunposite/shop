99a3919637acf83d18ede862102531a620fd7edb
Dias Kadyrov <kadirov.dias@mail.ru>
2021-08-03 23:04:00 +0600

    -

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,10 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-<<<<<<< HEAD
-
-=======
->>>>>>> aab5587022d17d52b00f8c5997266d2ec90ce0b9
 
 def index(request):
     # тут вернем шаблон
