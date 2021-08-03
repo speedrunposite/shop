0682c4c5fc2702a9a196eb60006e691580a30354
Dias Kadyrov <kadirov.dias@mail.ru>
2021-08-04 02:12:10 +0600

    asdasdasd

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -37,8 +37,41 @@ class TileAdmin(admin.ModelAdmin):
         return super().formfield_for_foreignkey(db_field, request, **kwargs)
 
 
+class StairAdminForm(ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['image'].help_text = mark_safe(
+            '<span style="color:red; font-size:14px;">Загружайте изображение не более {}x{} с минимальным разрешением {}x{} и размером не более 5MB.</span>'.format(
+            *Product.MAX_RESOLUTION, *Product.MIN_RESOLUTION))
+
+    def clean_image(self):
+        image = self.cleaned_data['image']
+        img = Image.open(image)
+        min_height, min_width = Product.MIN_RESOLUTION
+        max_height, max_width = Product.MAX_RESOLUTION
+        if image.size > Product.MAX_IMAGE_SIZE:
+            raise ValidationError('Размер изображения не должен превышать 5MB!')
+        if img.height < min_height or img.width < min_width:
+            raise ValidationError('Разрешение изображения меньше минимального!')
+        if img.height > max_height or img.width > max_width:
+            raise ValidationError('Разрешение изображения больше максимального!')
+        return image
+
+
+class StairAdmin(admin.ModelAdmin):
+
+    form = StairAdminForm
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == 'category':
+            return ModelChoiceField(Category.objects.filter(slug='stairs'))
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
+
 admin.site.register(Category)
 admin.site.register(Tile, TileAdmin)
+admin.site.register(Stair, StairAdmin)
 admin.site.register(CartProduct)
 admin.site.register(Cart)
 admin.site.register(Customer)
