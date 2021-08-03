06aeb1dbff7eb2b7e53dbf7add3f167a3a695914
Dias Kadyrov <kadirov.dias@mail.ru>
2021-08-03 23:03:25 +0600

    -

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,9 +1,31 @@
 from django.contrib import admin
-from django.forms import ModelChoiceField
+from django.forms import ModelChoiceField, ModelForm, ValidationError
 from .models import *
+from PIL import Image
+
+class TileAdminForm(ModelForm):
+
+    MIN_RESOLUTION = (450, 300)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['image'].help_text = 'Загружайте изображение с минимальным разрешением {}x{}'.format(
+            *self.MIN_RESOLUTION)
+
+    def clean_image(self):
+        image = self.cleaned_data['image']
+        img = Image.open(image)
+        min_height, min_width = self.MIN_RESOLUTION
+        if img.height < min_height or img.width < min_width:
+            raise ValidationError(
+                'Загруженное изображение: {}x{}. Минимально допустимое резрешение: {}x{}'.format(img.height, img.width, *self.MIN_RESOLUTION))
+        return image
+
 
 # с помощью такой конструкции мы исключаем лишние категории из выбора при создании продукта
 class TileAdmin(admin.ModelAdmin):
+
+    form = TileAdminForm
 
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
         if db_field.name == 'category':
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+<<<<<<< HEAD
 
+=======
+>>>>>>> aab5587022d17d52b00f8c5997266d2ec90ce0b9
 
 def index(request):
     # тут вернем шаблон
