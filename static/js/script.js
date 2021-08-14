1fcd61ebd6c577ec5c6f52fffe7fd9a371ee3200
ruslan.sibgatulin05@gmail.com <ruslan.sibgatulin05@gmail.com>
2021-08-15 00:15:59 +0600

    added smth

--- static/js/script.js
+++ static/js/script.js
@@ -1,13 +1,12 @@
 
 window.onscroll = function() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20){
+    if (document.body.scrollTop >= 20 || document.documentElement.scrollTop >= 20){
         scroll_up.style.display = 'block';
     }
     else{
         scroll_up.style.display = 'none';
     }
 }
-console.log('work');
 
 function topFunction() {
     document.body.scrollTop = 0;
