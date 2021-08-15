f1593417c698faab02f35f50b97c1f24f6ab727d
Dias Kadyrov <kadirov.dias@mail.ru>
2021-08-15 23:52:15 +0600

    фыв

--- static/js/script.js
+++ static/js/script.js
@@ -1,15 +1,4 @@
 
-<<<<<<< HEAD
-=======
-window.onscroll = function() {
-    if (document.body.scrollTop >= 20 || document.documentElement.scrollTop >= 20){
-        scroll_up.style.display = 'block';
-    }
-    else{
-        scroll_up.style.display = 'none';
-    }
-}
->>>>>>> 1fcd61ebd6c577ec5c6f52fffe7fd9a371ee3200
 
 function topFunction() {
     document.body.scrollTop = 0;
