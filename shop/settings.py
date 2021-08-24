bbd9cebbda71a89400487659971816a41c004ef8
teamcutter <ruslan.sibgatulin05@gmail.com>
2021-08-24 22:00:48 +0600

    sdsdsd

--- shop/settings.py
+++ shop/settings.py
@@ -76,16 +76,9 @@ WSGI_APPLICATION = 'shop.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # },
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'shop_db',
-        'USER' : 'root',
-        'PASSWORD' : 'nazaret8888',
-        'HOST' : 'localhost',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
